bec9fed863019a3e3edda084d478eabe30bacb38
vietvu-nguyen <vietvu.hcm@gmail.com>
2020-05-26 17:56:52 +0900

    Add ramda folktale methods

--- ramda/ramda_folktale.js
+++ ramda/ramda_folktale.js
@@ -24,5 +24,36 @@ const rHeadWithFolktaleResult = R.curry(
   (result) => withFolktaleResultChain(R.head, result),
 );
 
+
+/**
+ * Works like R.omit, return a folktale result
+ * @param  {Array} keys   - keys to omit
+ * @param  {FolktaleResult} result - the value must be an object
+ * @return {FolktaleResult}
+ */
+const rOmitWithFolktaleResult = R.curry(
+  (keys, result) => withFolktaleResultChain(R.omit(keys), result),
+);
+/**
+ * Works like R.pick, return a folktale result
+ * @param  {Array} keys   - keys to pick
+ * @param  {FolktaleResult} result - the value must be an object
+ * @return {FolktaleResult}
+ */
+const rPickWithFolktaleResult = R.curry(
+  (keys, result) => withFolktaleResultChain(R.pick(keys), result),
+);
+/**
+ * Works like R.merge, return a folktale result
+ * @param  {Object} firstObject   - first object to merge
+ * @param  {FolktaleResult} result - the value must be an object
+ * @return {FolktaleResult}
+ */
+const rMergeWithFolktaleResult = R.curry(
+  (firstObject, result) => withFolktaleResultChain(R.merge(firstObject), result),
+);
 self.rPropWithFolktaleResult = rPropWithFolktaleResult;
 self.rHeadWithFolktaleResult = rHeadWithFolktaleResult;
+self.rOmitWithFolktaleResult = rOmitWithFolktaleResult;
+self.rPickWithFolktaleResult = rPickWithFolktaleResult;
+self.rMergeWithFolktaleResult = rMergeWithFolktaleResult;
