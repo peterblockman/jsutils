bc4e103a09369c3c3a308267ec329959018ff2a2
vietvu-nguyen <vietvu.hcm@gmail.com>
2020-06-02 21:07:24 +0900

    Add isJsonApi

--- json_api/__test__/utils.test.js
+++ json_api/__test__/utils.test.js
@@ -6,7 +6,10 @@ const {
 } = require('./register_mocks');
 const {
   isJsonApiRegisteringSuccessful,
+  isJsonApi,
 } = require('../utils');
+const { jsonApiMockObject } = require('./include_mocks');
+
 const {
   createGerenateJsonApiRegister,
 } = require('../register');
@@ -21,6 +24,20 @@ describe('modules/json_api/utils', () => {
     it('Should return false when registering failed', () => {
       const Serializer = new JSONAPISerializer();
       const output = isJsonApiRegisteringSuccessful(Result.Ok(Serializer));
+      expect(output).toBe(false);
+    });
+  });
+  describe('utils/isJsonApi', () => {
+    it('Should return true when pass a jsonapi data', () => {
+      const output = isJsonApi(jsonApiMockObject);
+      expect(output).toBe(true);
+    });
+    it('Should return false when pass a non-jsonapi data', () => {
+      const output = isJsonApi({ foo: 1 });
+      expect(output).toBe(false);
+    });
+    it('Should return false when pass a empty object', () => {
+      const output = isJsonApi({});
       expect(output).toBe(false);
     });
   });
--- json_api/utils.js
+++ json_api/utils.js
@@ -6,7 +6,13 @@ const R = require('ramda');
 const Boom = require('@hapi/boom');
 const Result = require('folktale/result');
 const isEmpty = require('lodash/isEmpty');
+const isPlainObject = require('lodash/isPlainObject');
+const isArray = require('lodash/isArray');
 const { isInsatanceOfFolktaleResultOk } = require('../folktale/instances');
+const {
+  jsonApiKeys, jsonApiEssentialkeys, jsonApiDataEssentialKeys, jsonApiDataKeys,
+} = require('./constants');
+const { trace } = require('../ramda/trace');
 
 /**
  * Get error type: generic Js Error or Boom error object
@@ -41,5 +47,53 @@ const isJsonApiRegisteringSuccessful = R.curry(
     return !isEmpty(R.prop('schemas')(value));
   },
 );
+
+const handleGetJsonApiDataKeys = R.curry((data) => {
+  if (isPlainObject(data)) return R.keys(data);
+  if (isArray(data)) return R.map(R.keys, data);
+});
+const handleFlatten = (keys) => (!keys ? [] : R.flatten(keys));
+const getJsonApiDataKeys = R.curry(
+  (data) => R.pipe(
+    R.prop('data'),
+    handleGetJsonApiDataKeys,
+    handleFlatten,
+  )(data),
+);
+const createIsJsonApi = R.curry(
+  (
+    jsonApiKeys,
+    jsonApiEssentialkeys,
+    jsonApiDataKeys,
+    jsonApiDataEssentialKeys,
+    data,
+  ) => {
+    if (isEmpty(data)) return false;
+    const keys = R.pipe(
+      R.keys,
+      R.filter((x) => R.includes(x, jsonApiKeys)),
+    )(data);
+    const essentialKeys = R.filter((x) => R.includes(x, jsonApiEssentialkeys), keys);
+    const dataKeys = R.pipe(
+      getJsonApiDataKeys,
+      R.filter((x) => R.includes(x, jsonApiDataKeys)),
+    )(data);
+    const dataEssentialkeys = R.pipe(
+      getJsonApiDataKeys,
+      R.filter((x) => R.includes(x, jsonApiDataEssentialKeys)),
+    )(data);
+
+    const isKeysValid = !isEmpty(keys) && !isEmpty(essentialKeys);
+    const isDataKeysValid = !isEmpty(dataKeys) && !isEmpty(dataEssentialkeys);
+    return isKeysValid && isDataKeysValid;
+  },
+);
+const isJsonApi = createIsJsonApi(
+  jsonApiKeys,
+  jsonApiEssentialkeys,
+  jsonApiDataKeys,
+  jsonApiDataEssentialKeys,
+);
 self.getErrorType = getErrorType;
 self.isJsonApiRegisteringSuccessful = isJsonApiRegisteringSuccessful;
+self.isJsonApi = isJsonApi;
