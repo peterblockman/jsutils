b4d693b8c5679b1cd9ba6ab6b39e702afa7ec683
peterblockman <vietvu.hcm@gmail.com>
2020-08-20 20:26:50 +0900

    Add unwrapResultList

--- folktale/instances.js
+++ folktale/instances.js
@@ -1,5 +1,12 @@
+class self {
+
+}
+module.exports = self;
 const R = require('ramda');
 const Result = require('folktale/result');
+const isArray = require('lodash/isArray');
+const isPlainObject = require('lodash/isPlainObject');
+const Boom = require('@hapi/boom');
 
 const instances = {
   Ok: Result.Ok,
@@ -11,8 +18,77 @@ const createIsInstaneOfFolktaleResult = R.curry(
 const isInsatanceOfFolktaleResult = createIsInstaneOfFolktaleResult(instances);
 const isInsatanceOfFolktaleResultOk = isInsatanceOfFolktaleResult('Ok');
 const isInsatanceOfFolktaleResultError = isInsatanceOfFolktaleResult('Error');
+const handleIsEveryItemOk = R.curry(
+  (data) => {
+    if (isArray(data)) {
+      return R.all(isInsatanceOfFolktaleResultOk, data);
+    }
+    return false;
+  },
+);
+const isEveryItemOk = R.curry((result) => {
+  if (!Result.hasInstance(result)) return handleIsEveryItemOk(result);
+  result.chain((data) => handleIsEveryItemOk(data));
+});
 
-module.exports = {
-  isInsatanceOfFolktaleResultOk,
-  isInsatanceOfFolktaleResultError,
-};
+const hanleUnwrapResultList = R.curry(
+  (data) => {
+    if (!isArray(data)) {
+      return data;
+    }
+    if (isEveryItemOk(data)) {
+      return R.map(
+        (item) => item.merge(),
+        data,
+      );
+    }
+    if (R.any(isInsatanceOfFolktaleResultError, data)) {
+      // return the error message
+      return R.pipe(
+        R.filter(isInsatanceOfFolktaleResultError),
+        R.map((item) => {
+          const values = item.merge();
+          if (values instanceof Error) {
+            return values.message;
+          }
+          if (Boom.isBoom(values)) {
+            return R.view(
+              R.lensPath(['output', 'payload', 'message']),
+              values,
+            );
+          }
+          return values;
+        }),
+        R.join(' '),
+      )(data);
+    }
+    // mixed between Ok and normal data type
+    return R.map(
+      (item) => (
+        isInsatanceOfFolktaleResultOk(item)
+          ? item.merge()
+          : item
+      ),
+      data,
+    );
+  },
+);
+/**
+ * Unwrap folttake result list
+ * if resultInput's data is an array, unwrap all item in the array then:
+ * - return an array of unwraped item, there is no Error
+ * - return a string message, if there is an Error
+ * if esultInput's data is an array, return the data
+ * @param  {FolktaleResult|Array} resultInput
+ * @return {any}
+ */
+const unwrapResultList = R.curry(
+  (resultInput) => {
+    if (!Result.hasInstance(resultInput)) return hanleUnwrapResultList(resultInput);
+    return resultInput.chain((data) => hanleUnwrapResultList(data));
+  },
+);
+self.isInsatanceOfFolktaleResultOk = isInsatanceOfFolktaleResultOk;
+self.isInsatanceOfFolktaleResultError = isInsatanceOfFolktaleResultError;
+self.isEveryItemOk = isEveryItemOk;
+self.unwrapResultList = unwrapResultList;
